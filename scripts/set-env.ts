dfb8cc13faa7823e74f082506b032d5ab7f46187
Eugen Schereisin <eugen.schereisin@gmail.com>
2018-03-01 03:29:16 +0100

    first attempt

---
+++ scripts/set-env.ts
@@ -0,0 +1,29 @@
+// https://medium.com/@natchiketa/angular-cli-and-os-environment-variables-4cfa3b849659
+
+
+import { writeFile } from 'fs';
+// import { argv } from 'yargs';
+
+// This is good for local dev environments, when it's better to
+// store a projects environment variables in a .gitignore'd file
+// require('dotenv').config();
+
+// Would be passed to script like this:
+// `ts-node set-env.ts --environment=dev`
+// we get it from yargs's argv object
+// const environment = argv.environment;
+// const isProd = environment === 'prod';
+
+const targetPath = `./src/environments/externalsettings.json`;
+const envConfigFile = `
+{
+  "name": "${process.env.SUPER_SECRET_NAME}"
+}
+`
+writeFile(targetPath, envConfigFile, function (err) {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log(`Output generated at ${targetPath}`);
+});
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
-export const environment = {
-  production: false
+declare var require: any;
+
+const environmentSettings = {
+  production: false,
+  name: 'dev'
 };
+
+const json = require('./externalsettings.json');
+
+Object.assign(environmentSettings, json);
+
+export const environment = environmentSettings;
